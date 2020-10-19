ab453c03e5fada065b6840cb7c0155b82a28c609
airi-14x <airi.14x@gmail.com>
2020-10-19 16:37:45 -0400

    Complete all available parts and Update Doc. Waiting for Challenge Queries

--- Project2/query.py
+++ Project2/query.py
@@ -10,11 +10,17 @@ def querying(input_file, query, *args):
     text = ""
     with open(input_file) as file:
         text = file.read()
-
+    output_file = ""
+    for ar in args:
+        output_file = ar
     index = json.loads(text)
+    query_result = {}
     try:
         print("Result:")
         print(index[query])
+        query_result[query] = index[query]
+        json.dump(query_result, open(
+            output_file, "a", encoding="utf-8"), indent=3)
     except KeyError as err:
         print(err.args)
         print(query + " is not in the index.")
@@ -68,7 +74,8 @@ def create_table_non_positional_index(input_file, query, *args):
         string = " "
         remove_duplicate_doc_id = remove_duplicate_doc_id.split(" ")
         remove_duplicate_doc_id = [float(i) for i in remove_duplicate_doc_id]
-        remove_duplicate_doc_id = sorted(remove_duplicate_doc_id)  # Sorted but as float instead of string
+        # Sorted but as float instead of string
+        remove_duplicate_doc_id = sorted(remove_duplicate_doc_id)
 
         for index, elements in enumerate(remove_duplicate_doc_id):
             remove_duplicate_doc_id[index] = str(elements)
@@ -84,7 +91,8 @@ def create_table_non_positional_index(input_file, query, *args):
     for final_casefold_terms, final_casefold_doc_id in index3.items():
         string = " "
         count_term = count_term + 1
-        count_doc_id = count_doc_id + len(string.join(final_casefold_doc_id).split(" "))
+        count_doc_id = count_doc_id + \
+            len(string.join(final_casefold_doc_id).split(" "))
 
     print("# of Terms: (Casefold) " + str(count_term))
     print("# of Doc IDs: (Casefold) " + str(count_doc_id))
@@ -96,12 +104,12 @@ def create_table_non_positional_index(input_file, query, *args):
     stopwords = []
     stopwords_doc = ""
     if stopwords_file is not None:
-        stopwords_file = open(stopwords_file,'r')
+        stopwords_file = open(stopwords_file, 'r')
         for line in stopwords_file:
             current_line = line.split(" ")
             stopwords.append(current_line[1].strip())
 
-    #print(stopwords)
+    # print(stopwords)
     count_term = 0
     count_doc_id = 0
 
@@ -110,11 +118,15 @@ def create_table_non_positional_index(input_file, query, *args):
     for non_stopword_terms, non_stopword_doc_id in index3.items():
         if non_stopword_terms not in stopwords:
             count_term = count_term + 1
-            count_doc_id = count_doc_id + len(string.join(non_stopword_doc_id).split(" "))
-            non_stopword_doc_id = [float(id_number) for id_number in non_stopword_doc_id]
-            non_stopword_doc_id = sorted(non_stopword_doc_id)  # Sorted but as float instead of string
+            count_doc_id = count_doc_id + \
+                len(string.join(non_stopword_doc_id).split(" "))
+            non_stopword_doc_id = [float(id_number)
+                                   for id_number in non_stopword_doc_id]
+            # Sorted but as float instead of string
+            non_stopword_doc_id = sorted(non_stopword_doc_id)
             for index_stopwords, elements_stopwords in enumerate(non_stopword_doc_id):
-                non_stopword_doc_id[index_stopwords] = str(int(elements_stopwords))
+                non_stopword_doc_id[index_stopwords] = str(
+                    int(elements_stopwords))
 
             final_compressed_dictionary[non_stopword_terms] = non_stopword_doc_id
 
@@ -130,15 +142,15 @@ def create_table_non_positional_index(input_file, query, *args):
         print(query + " is not in the index.")
 
 
-
 parser = argparse.ArgumentParser(
     description='Process input path parameter', formatter_class=RawTextHelpFormatter)
 parser.add_argument('-i', '--input_file', type=str)
-parser.add_argument('-o', '--output_file', type=str, default=None)
+parser.add_argument('-o', '--output_file', type=str,
+                    default='sampleQueries.json')
 parser.add_argument('-s', '--stopwords', default=None)
 parser.add_argument('-q', '--query', type=str, default='!')
 
 args = parser.parse_args()
 # print(args.input_file)
 querying(args.input_file, args.query, args.output_file)
-create_table_non_positional_index(args.input_file, args.query, args.stopwords)
+#create_table_non_positional_index(args.input_file, args.query, args.stopwords)
