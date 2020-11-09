793774dbb1caea89cec4841230e9252b4d0c0d0b
airi-14x <airi.14x@gmail.com>
2020-11-09 09:48:40 -0500

    Parse DocID and term

--- Project3/spimi-indexer.py
+++ Project3/spimi-indexer.py
@@ -11,6 +11,30 @@ def build_index(input_file, output_file):
         for line in file:
             lines.append(line)
 
+    #current_dictionary = {}
+    #while len(current_dictionary) < 10:
+    iteration = 0;
+    while iteration < 2:
+        for line in lines:
+            line_arr = line.split(",")
+            #print(line_arr)
+            #print(line_arr[0])
+            #print(line_arr[1])
+            doc_id_component = line_arr[0]
+            term_component = line_arr[1]
+            doc_id_element = doc_id_component.split("\"")
+            term_element = term_component.split("\"")
+            #print(doc_id_element[0])
+            #print(doc_id_element[1]) # Actual docID
+            #print(term_element[0])
+            #print(term_element[1])
+            doc_id = doc_id_element[1]
+            term = term_element[1]
+            print(doc_id)
+            print(term)
+
+
+            iteration = iteration + 1
     #print(lines[4])
     #json.dump(lines, open(
     #    output_file, "w", encoding="utf-8"), indent=3)
