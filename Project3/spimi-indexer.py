7bdc58f09a938823c0fbe4211e3c36e172879bd0
airi-14x <airi.14x@gmail.com>
2020-11-09 09:58:20 -0500

    Add 500 term block with dups. Need to merge docIDS for existing terms

--- Project3/spimi-indexer.py
+++ Project3/spimi-indexer.py
@@ -11,30 +11,23 @@ def build_index(input_file, output_file):
         for line in file:
             lines.append(line)
 
-    #current_dictionary = {}
-    #while len(current_dictionary) < 10:
-    iteration = 0;
-    while iteration < 2:
-        for line in lines:
-            line_arr = line.split(",")
-            #print(line_arr)
-            #print(line_arr[0])
-            #print(line_arr[1])
-            doc_id_component = line_arr[0]
-            term_component = line_arr[1]
-            doc_id_element = doc_id_component.split("\"")
-            term_element = term_component.split("\"")
-            #print(doc_id_element[0])
-            #print(doc_id_element[1]) # Actual docID
-            #print(term_element[0])
-            #print(term_element[1])
-            doc_id = doc_id_element[1]
-            term = term_element[1]
-            print(doc_id)
-            print(term)
+
+    current_dictionary = {}
+    for line in lines:
+        line_arr = line.split(",")
+        doc_id_component = line_arr[0]
+        term_component = line_arr[1]
+        doc_id_element = doc_id_component.split("\"")
+        term_element = term_component.split("\"")
+        doc_id = doc_id_element[1] # Actual docID
+        term = term_element[1]
+
+        current_dictionary[term] = doc_id
+        if len(current_dictionary) > 500:
+            print(current_dictionary)
+            break
 
 
-            iteration = iteration + 1
     #print(lines[4])
     #json.dump(lines, open(
     #    output_file, "w", encoding="utf-8"), indent=3)
